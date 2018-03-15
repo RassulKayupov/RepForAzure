47f90b067031def49130ba9f5ff67d627dc543f9
Rassul Kayupov <rassul_kayupov@epam.com>
2018-03-15 14:02:35 +0600

    NewApp
    NewApp

---
+++ RK_MyWebApp/Models/ILogRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace RK_MyWebApp.Models
+{
+    public interface ILogRepository<T> where T : ITableEntity, new()
+    {
+        void Add(IEnumerable<T> obj);
+    }
+}
